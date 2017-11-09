63c046bb2eed4cd556b676d45782aad7855c396d
David Welch <dmw9400@gmail.com>
2017-11-09 18:50:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,11 +5,11 @@ end
 # def batch_badge_creator(name_array)
 #   i = 0
 #   new_array = []
-#   
+#
 #   while i < name_array.length
 #     new_array.push("Hello, my name is #{name_array[i]}")
 #     i++
 #   end
-#   
+#
 #   return new_array
 # end
